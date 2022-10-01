60cc38cd3f858d22d540d9cdd52596b93e20345c
Aubree Herrion <aubreeherrion@gmail.com>
2022-10-01 08:31:49 -0500

    created detail function that have details for poster show on DOM

--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -1,8 +1,30 @@
-import {useEffect} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
 
-function movieDetail (){
-    
+function MovieDetail (){
+    const movie = useSelector(store => store.selectMovie);
+    const genres = useSelector(store => store.genres);
+    const history = useHistory()
+
+    return(
+        <div>
+            <h2>
+                {movie.title}
+            </h2>
+            <img src={movie.poster} />
+            <h3>
+                Movie Details
+            </h3>
+            <p>
+                {movie.description}
+            </p>
+            <h3>
+                Genres:
+            </h3>
+            {genres.map(genre => `${genre.name}`)}
+        </div>
+    )
 }
+
+export default MovieDetail;
